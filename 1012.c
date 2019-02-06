4711c3ac7cf770a4bd9f930855fc85232f2d0a0e
SAIRENT0 <39720118+SAIRENT0@users.noreply.github.com>
2019-02-06 14:55:01 +0800

    Create 1012.c

---
+++ 1012.c
@@ -0,0 +1,45 @@
+/*
+//C语言程序设计教程（第三版）课后习题6.2
+//题目描述
+输入一行字符，分别统计出其中英文字母、数字、空格和其他字符的个数。
+//输入
+一行字符
+//输出
+统计值
+//样例输入
+aklsjflj123 sadf918u324 asdf91u32oasdf/.';123
+//样例输出
+23 16 2 4
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int letters = 0, numbers = 0, spaces = 0, other = 0;
+	char ch;
+
+	while(1)
+	{
+		ch = getchar();
+		if(ch == '\n')
+			break;
+
+		if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+			letters++;
+
+		else if(ch >= '0' && ch <= '9')
+			numbers++;
+
+		else if(ch == ' ')
+			spaces++;
+
+		else
+			other++;
+
+	}
+
+	printf("%d %d %d %d\n", letters, numbers, spaces, other);
+
+   return 0;
+}
